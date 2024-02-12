f8a3f83ead5068dab624adeee354eeb063c4ffc1
berk.ozzambak <berkozzambak@gmail.com>
2024-02-13 00:54:00 +0100

    Add quiz db model

--- database/db_models.py
+++ database/db_models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, String, Boolean, DateTime, BIGINT
+from sqlalchemy.dialects.mysql import LONGTEXT
 from sqlalchemy.sql import func
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -12,3 +13,12 @@ class User(Base):
     password = Column(String, nullable=False, unique=True)
     is_active = Column(Boolean, nullable=False, default=1)
     member_since = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+
+class Quiz(Base):
+    __tablename__ = 'Quiz'
+    quiz_id = Column(BIGINT, primary_key=True, autoincrement=True)
+    quiz_title = Column(String, nullable=False)
+    quiz_description = Column(LONGTEXT, nullable=True)
+    meta_prompt = Column(LONGTEXT, nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+    is_active = Column(Boolean, nullable=False, default=1)
