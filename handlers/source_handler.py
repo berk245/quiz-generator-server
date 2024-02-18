05e804f9ad9d4e68eb4d433d2176e667d7e6a14c
berk.ozzambak <berkozzambak@gmail.com>
2024-02-18 21:55:34 +0100

    feat: implement source handler methods

---
+++ handlers/source_handler.py
@@ -0,0 +1,46 @@
+from fastapi import Request, UploadFile
+from fastapi.responses import JSONResponse
+from sqlalchemy.orm import Session
+
+from database.db_models import Source, QuizSource
+# from helpers.quiz_helpers import serialize_quiz
+
+
+def get_sources(request: Request, db: Session):
+    user_id = request.state.user_id
+    sources = db.query(Source).filter(Source.user_id == user_id).all()
+
+    return JSONResponse(status_code=200, content={"data": sources})
+
+
+def add_source(user_id: str, file: UploadFile, db: Session ):
+
+    new_source = Source(
+        file_name=file.filename,
+        user_id=user_id,
+        file_hash='123'
+        # ToDo create file hash
+    )
+
+    db.add(new_source)
+    db.commit()
+
+    db.refresh(new_source)
+
+    return new_source
+
+
+def add_quiz_source(new_source: Source, quiz_id: int, db: Session):
+    new_quiz_source = QuizSource(
+        source_id=new_source.source_id,
+        quiz_id=quiz_id,
+    )
+
+    db.add(new_quiz_source)
+    db.commit()
+
+    db.refresh(new_quiz_source)
+
+    return new_quiz_source
+
+
