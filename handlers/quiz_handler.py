35b9468f1bcde06cef5d47941494cc8062e08687
berk.ozzambak <berkozzambak@gmail.com>
2024-02-22 13:53:15 +0100

    feat: implement quiz creation method

--- handlers/quiz_handler.py
+++ handlers/quiz_handler.py
@@ -1,10 +1,16 @@
+from dotenv import load_dotenv
 from fastapi import Request, UploadFile
 from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
+from starlette.exceptions import HTTPException
 
 from database.db_models import Quiz
 from helpers.quiz_helpers import serialize_quiz
-from . import  source_handler
+
+from . import source_handler
+from helpers.quiz_helpers import get_file_hash
+from helpers.vectorstore_helpers import add_quiz_to_vectorstore
+
 
 def get_quizzes(request: Request, db: Session):
     user_id = request.state.user_id
@@ -16,27 +22,34 @@ def get_quizzes(request: Request, db: Session):
 
 
 async def add_quiz(request: Request, source_file: UploadFile, db: Session):
-    quiz_info = await request.form()
+    load_dotenv()
+    try:
+        quiz_info = await request.form()
+        file_hash = get_file_hash(source_file.file)
+        
+        # Create quiz in DB
+        new_quiz = Quiz(
+            quiz_title=quiz_info.get('quiz_title'),
+            quiz_description=quiz_info.get('quiz_description'),
+            keywords=quiz_info.get('keywords'),
+            meta_prompt=quiz_info.get('meta_prompt'),
+            user_id=request.state.user_id
+        )
+        db.add(new_quiz)
+        db.commit()
+        db.refresh(new_quiz)
 
-    # Create quiz
-    new_quiz = Quiz(
-        quiz_title=quiz_info.get('quiz_title'),
-        quiz_description=quiz_info.get('quiz_description'),
-        keywords=quiz_info.get('keywords'),
-        meta_prompt=quiz_info.get('meta_prompt'),
-        user_id=request.state.user_id
-    )
-    db.add(new_quiz)
-    db.commit()
-    db.refresh(new_quiz)
+        # Create source in DB
+        new_source = source_handler.add_source(user_id=request.state.user_id, file=source_file, file_hash=file_hash,
+                                               db=db)
+        # Connect the source with the quiz by creating a QuizSource table
+        source_handler.add_quiz_source(new_source=new_source, quiz_id=new_quiz.quiz_id, db=db)
+        
+        add_quiz_to_vectorstore(source_file=source_file, new_quiz=new_quiz, file_hash=file_hash)
+        return JSONResponse(status_code=200, content={"quiz_id": new_quiz.quiz_id})
 
-    # Add source
-    new_source = source_handler.add_source(user_id=request.state.user_id,
-                                           file=source_file,
-                                           db=db)
-
-    # Connect the source with the quiz by creating a QuizSource table
-    source_handler.add_quiz_source(new_source=new_source, quiz_id=new_quiz.quiz_id, db=db)
-
-    return JSONResponse(status_code=200, content={"quiz_id": new_quiz.quiz_id})
-
+    except Exception as e:
+        # Todo: implement solid rollback method
+        print(e)
+        db.rollback()
+        raise HTTPException(status_code=500, detail='Internal server error')
