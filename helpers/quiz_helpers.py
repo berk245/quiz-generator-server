5472a60b9a334ed2f82af2d30b5b2623e4b8eca6
berk.ozzambak <berkozzambak@gmail.com>
2024-02-22 13:25:53 +0100

    feat: implement file hash getter method

--- helpers/quiz_helpers.py
+++ helpers/quiz_helpers.py
@@ -1,3 +1,5 @@
+import hashlib
+
 def serialize_quiz(quiz):
     return {
         "quiz_id": quiz.quiz_id,
@@ -8,3 +10,14 @@ def serialize_quiz(quiz):
         "created_at": quiz.created_at.isoformat() if quiz.created_at else None,
         "is_active": quiz.is_active,
     }
+
+
+def get_file_hash(file: any, algorithm: str = 'sha256') -> str:
+    hash_obj = hashlib.new(algorithm)
+    if isinstance(file, str):
+        hash_obj.update(file.encode())
+    else:
+        for chunk in iter(lambda: file.read(4096), b''):
+            hash_obj.update(chunk)
+        file.seek(0)
+    return hash_obj.hexdigest()
