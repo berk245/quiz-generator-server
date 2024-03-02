e504d5a99d414446a4aa2bca839ec1b5e3d3105d
berk.ozzambak <berkozzambak@gmail.com>
2024-03-02 03:29:22 +0100

    feat: add conversational chain getter with open ai functions

--- helpers/vectorstore_helpers.py
+++ helpers/vectorstore_helpers.py
@@ -1,17 +1,21 @@
 import os
-
+from typing import List
+from fastapi import UploadFile
+from database.db_models import Quiz
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_pinecone import Pinecone
-from fastapi import  UploadFile
-
-from database.db_models import Quiz
+from langchain.output_parsers.openai_tools import JsonOutputToolsParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain_openai import ChatOpenAI
 from loaders import PDFMinerPagesLoader
 
 
-def add_quiz_to_vectorstore(source_file: UploadFile, new_quiz: Quiz, file_hash: str ):
+def add_quiz_to_vectorstore(source_file: UploadFile, new_quiz: Quiz, file_hash: str):
     
-    documents=get_documents_from_file(source_file=source_file, file_hash=file_hash)
+    documents = get_documents_from_file(source_file=source_file, file_hash=file_hash)
     index_name, namespace, embeddings = get_pinecone_config(quiz=new_quiz)
     
     vectorstore = Pinecone.from_documents(
@@ -32,11 +36,57 @@ def get_pinecone_config(quiz: Quiz):
     namespace = os.getenv("PINECONE_NAMESPACE") + f'{quiz.quiz_id}'
     embeddings = OpenAIEmbeddings(model='text-embedding-ada-002')
     
-    return index_name, namespace , embeddings
+    return index_name, namespace, embeddings
 
 
 def get_documents_from_file(source_file: UploadFile, file_hash: str):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200, length_function=len, is_separator_regex=False)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200, length_function=len,
+                                                   is_separator_regex=False)
     split_documents = text_splitter.split_documents(_get_raw_pdf_text(source_file=source_file, file_hash=file_hash))
 
     return split_documents
+
+
+def _get_retriever(quiz: Quiz):
+    index_name, namespace, embeddings = get_pinecone_config(quiz=quiz)
+    vectorstore = Pinecone.from_existing_index(
+        index_name=index_name,
+        namespace=namespace,
+        embedding=embeddings
+    )
+    retriever = vectorstore.as_retriever()
+    return retriever
+
+
+class GeneratedQuestion(BaseModel):
+    question_id: str = Field(description='A unique identifier for each generated question. Could be the index.')
+    question_text: str = Field(description="The question to be added to the quiz")
+    multiple_choices: str = Field(description="A string that includes four, comma separated potential answers to the "
+                                              "question")
+    correct_answer: str = Field(description='Correct answer to the question. Must be one of the four multiple choices.')
+    question_type: str = Field(description='Always set it as "multi"')
+
+
+class GeneratedQuestions(BaseModel):
+    questions: List[GeneratedQuestion] = Field(description="List of generated questions in the specified amount")
+
+
+def get_conversation_chain(quiz: Quiz):
+    retriever = _get_retriever(quiz=quiz)
+
+    template = """Answer the question based only on the following context:
+    {context}
+
+    Question: {question}
+    """
+    prompt = ChatPromptTemplate.from_template(template)
+    model = ChatOpenAI().bind_tools([GeneratedQuestions])
+
+    chain = (
+        {"context": retriever, 'question': RunnablePassthrough()}
+        | prompt
+        | model
+        | JsonOutputToolsParser()
+    )
+
+    return chain
