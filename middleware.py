b5ff544e7828efd59a2bf1b8ff5e6fbb85199855
berk.ozzambak <berkozzambak@gmail.com>
2024-03-15 15:28:36 +0100

    feat: improve log request midware

--- middleware.py
+++ middleware.py
@@ -3,11 +3,13 @@ from fastapi.responses import JSONResponse
 from helpers.auth_helpers import validate_jwt
 from cloudwatch_logger import cloudwatch_logger
 from fastapi import Request
+import json
+
 
 def setup_cors_middleware(app) -> None:
     app.add_middleware(
         CORSMiddleware,
-        # allow_origins=['*'],
+        allow_origins=['*'],
         allow_credentials=True,
         allow_headers=['Authorization'],
         allow_methods=['*']
@@ -25,23 +27,53 @@ async def validate_token(request, call_next) -> JSONResponse:
             # attach extracted user info to request for future access
             request.state.user_id = user_id
 
-    except:
+            response = await call_next(request)
+            return response
+    except Exception as e:
+        print(f'Error in token validator midware. {e}')
+        cloudwatch_logger.error(f'Error in token validator midware. \n'
+                                f'Token: {request.headers.get("authorization")} \n'
+                                f'Exception: {e}')
         return JSONResponse(status_code=401,
                             content={'error': 'Invalid or expired token'},
                             headers={'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': '*',
                                      'Access-Control-Allow-Methods': '*'})
-    try:
-        response = await call_next(request)
-        return response
-    except Exception as e:
-        print('Uncaught exception while processing a request:', e)
-
-    return JSONResponse(status_code=500, content={'error': 'Internal server error'},
-                        headers={'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': '*',
-                                 'Access-Control-Allow-Methods': '*'})
 
 
 async def log_request(request: Request, call_next):
-    cloudwatch_logger.info(f"Received request: {request.method} {request.url}")
-    response = await call_next(request)
-    return response
+    try:
+        if request.method == "OPTIONS":
+            # Check if it's a preflight request (OPTIONS method) and ignore it
+            return await call_next(request)
+
+        log_message = f"Received request: {request.method} {request.url}"
+
+        if request.method == "GET" and request.query_params:
+            log_message += f"\nURL parameters: {request.query_params}"
+
+        elif request.method == "POST":
+            body = await request.body()
+            # Check if the content type is JSON, handle form request separately
+            if "application/json" in request.headers.get("content-type", ""):
+                try:
+                    body_json = json.loads(body)
+                    if request.url.path in ["/login", "/signup"]:
+                        log_message = f"Auth request: {request.url.path}, email: {body_json.get('email')} "
+                    else:
+                        log_message += f"\nRequest body (JSON): {body_json}"
+                except json.JSONDecodeError:
+                    log_message += f"\nRequest body (JSON): Unable to parse JSON"
+
+        cloudwatch_logger.info(log_message)
+
+        # Call the next middleware or handler
+        response = await call_next(request)
+        return response
+
+    except Exception as e:
+        cloudwatch_logger.info(f'Request log failed. \n'
+                               f'Exception: {e}')
+        # Call the next middleware or handler
+        response = await call_next(request)
+
+        return response
